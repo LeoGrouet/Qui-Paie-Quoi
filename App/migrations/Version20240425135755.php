002f06d636dfb8835ec293c15b8b748c0c6b3551
Léo Grouet <111077650+LeoGrouet@users.noreply.github.com>
2024-04-26 09:15:24 +0200

    Feature/entity expense (#2)
    * chore: install BDD
    
    * Revert "chore: install BDD"
    
    This reverts commit 819ddba80c9a7cb28689b1c676983a3bd0ef57f0.
    
    * chore: install BDD
    
    * feat: Command app:handle-balance done

---
+++ App/migrations/Version20240425135755.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240425135755 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create expense table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE SEQUENCE expense_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE expense (id INT NOT NULL, amount INT NOT NULL, payer VARCHAR(60) NOT NULL, participants TEXT NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN expense.participants IS \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE expense_id_seq CASCADE');
+        $this->addSql('DROP TABLE expense');
+    }
+}
