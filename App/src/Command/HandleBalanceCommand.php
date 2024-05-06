c82c0ebde0e37d14944f66424106a3183928e512
LeoGrouet <leo.grouet@gmail.com>
2024-05-06 09:43:01 +0200

    feat: able to select a scenario

--- App/src/Command/HandleBalanceCommand.php
+++ App/src/Command/HandleBalanceCommand.php
@@ -4,11 +4,14 @@ namespace App\Command;
 
 use App\Entity\Expense;
 use App\Repository\ExpenseRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(name: 'app:handle-balance')]
 class HandleBalanceCommand extends Command
@@ -29,123 +32,76 @@ class HandleBalanceCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        //Scénario 1
-        $expenses = [
-            $expense = new Expense(9 * 100, "Alice", ["Alice", "Charles", "Camille"], "Bouteille  d'eau"),
-            $expense = new Expense(6 * 100, "Charles", ["Charles"], "Sandwich"),
-            $expense = new Expense(12 * 100, "Charles", ["Alice", "Camille"], "Nourriture"),
-            $expense = new Expense(36 * 100, "Camille", ["Alice", "Charles", "Camille"], "Essence")
-        ];
-        foreach ($expenses as $expense) {
+        $io = new SymfonyStyle($input, $output);
+        $scenario = $io->choice(
+            'Selectionner le scénario à executer:',
+            [
+                "First",
+                "Second",
+                "Third",
+                "Fourth"
+            ],
+            multiSelect: true
+        )[0];
+
+        $datas = [];
+
+        switch ($scenario) {
+            case 'First':
+                array_push(
+                    $datas,
+                    $expense = new Expense(9 * 100, "Alice", ["Alice", "Charles", "Camille"], "Bouteille  d'eau"),
+                    $expense = new Expense(6 * 100, "Charles", ["Charles"], "Sandwich"),
+                    $expense = new Expense(12 * 100, "Charles", ["Alice", "Camille"], "Nourriture"),
+                    $expense = new Expense(36 * 100, "Camille", ["Alice", "Charles", "Camille"], "Essence")
+                );
+                break;
+
+            case 'Second':
+                array_push(
+                    $datas,
+                    $expense = new Expense(10 * 100, "Pierre", ["David", "Emilie", "Florence"], "Taxi")
+                );
+                break;
+
+            case 'Third':
+                array_push(
+                    $datas,
+                    $expense = new Expense(10 * 100, "George", ["George", "Helene"], "Petit dèj"),
+                    $expense = new Expense(15 * 100, "Helene", ["George"], "Déjeuner"),
+                    $expense = new Expense(20 * 100, "George", ["Helene"], "Diner"),
+                );
+                break;
+
+            case 'Fourth':
+                array_push(
+                    $datas,
+                    $expense = new Expense(50 * 100, "Isabelle", ["Isabelle", "Julien", "Leo"], "Peinture"),
+                    $expense = new Expense(50 * 100, "Julien", ["Isabelle", "Julien", "Leo"], "Faux gazon"),
+                    $expense = new Expense(50 * 100, "Leo", ["Isabelle", "Julien", "Leo"], "Plomb"),
+                );
+                break;
+            default:
+                echo "Ce scénario n'existe pas. Veuillez indiquer : first ,second, third ou fourth";
+                return Command::FAILURE;
+        }
+
+
+        foreach ($datas as $expense) {
             $this->entityManager->persist($expense);
         }
         $this->entityManager->flush();
 
-        // // Scénario 2
-        // $expense = new Expense(10 * 100, "Pierre", ["David", "Emilie", "Florence"], "Taxi");
-        // $this->entityManager->persist($expense);
-        // $this->entityManager->flush();
-
-        // //Scénario 3
-        // $expenses = [
-        //     $expense = new Expense(10 * 100, "George", ["George", "Helene"], "Petit dèj"),
-        //     $expense = new Expense(15 * 100, "Helene", ["George"], "Déjeuner"),
-        //     $expense = new Expense(20 * 100, "George", ["Helene"], "Diner"),
-        // ];
-        // foreach ($expenses as $expense) {
-        //     $this->entityManager->persist($expense);
-        // }
-        // $this->entityManager->flush();
-
-
-        // //Scénario 4
-        // $expenses = [
-        //     $expense = new Expense(50 * 100, "Isabelle", ["Isabelle", "Julien", "Leo"], "Peinture"),
-        //     $expense = new Expense(50 * 100, "Julien", ["Isabelle", "Julien", "Leo"], "Faux gazon"),
-        //     $expense = new Expense(50 * 100, "Leo", ["Isabelle", "Julien", "Leo"], "Plomb"),
-        // ];
-        // foreach ($expenses as $expense) {
-        //     $this->entityManager->persist($expense);
-        // }
-        // $this->entityManager->flush();
-
         $expenses = $this->expenseRepository->findAll();
 
-        //Scénario 1
-        $users = [
-            [
-                "name" => "Alice",
-                "cost" => 0,
-                "participation" => 0,
-            ],
-            [
-                "name" => "Charles",
-                "cost" => 0,
-                "participation" => 0,
-            ],
-            [
-                "name" => "Camille",
-                "cost" => 0,
-                "participation" => 0,
-            ]
-        ];
+        $users = [];
 
-        // //Scénario 2
-        // $users = [
-        //     [
-        //         "name" => "Pierre",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        //     [
-        //         "name" => "David",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        //     [
-        //         "name" => "Emilie",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        //     [
-        //         "name" => "Florence",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        // ];
-
-        // //Scénario 3
-        // $users = [
-        //     [
-        //         "name" => "George",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        //     [
-        //         "name" => "Helene",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        // ];
-
-        // //Scénario 4
-        // $users = [
-        //     [
-        //         "name" => "Isabelle",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        //     [
-        //         "name" => "Julien",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        //     [
-        //         "name" => "Leo",
-        //         "cost" => 0,
-        //         "participation" => 0,
-        //     ],
-        // ];
+        foreach ($expenses as $expense) {
+            $users[$expense->getPayer()] = ["name" => $expense->getPayer(), "cost" => 0, "participation" => 0];
+            foreach ($expense->getParticipants() as $participant) {
+                $users[$participant] = ["name" => $participant, "cost" => 0, "participation" => 0];
+            }
+        };
 
         foreach ($expenses as $expense) {
             $amount = $expense->getAmount();
@@ -183,7 +139,7 @@ class HandleBalanceCommand extends Command
                 echo $user["name"] . " ne doit rien . Balance = " . $user["balance"] / 100 . " euros" . PHP_EOL;
             }
         }
-
+        echo "Il reste " . ($rest / 100) . " à répartir";
 
         foreach ($expenses as $expense) {
             $this->entityManager->remove($expense);
