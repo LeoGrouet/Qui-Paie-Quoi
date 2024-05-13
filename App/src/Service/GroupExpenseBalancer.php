e44d1d3802bf13458c5203cacf1d51187bebec6b
LeoGrouet <leo.grouet@gmail.com>
2024-05-13 12:05:55 +0200

    feat: initialize owe with good values

--- App/src/Service/GroupExpenseBalancer.php
+++ App/src/Service/GroupExpenseBalancer.php
@@ -22,13 +22,13 @@ class GroupExpenseBalancer
         }, []);
 
         $this->setExpenses($expenses, $bilans);
-
+        dump($bilans);
         return $bilans;
     }
 
     private function setExpenses($expenses, $bilans)
     {
-        $names = [];
+
         foreach ($expenses as $expense) {
             $amount = $expense->getAmount();
             $participants = $expense->getParticipants();
@@ -41,36 +41,83 @@ class GroupExpenseBalancer
                 $names[] = $participant;
             }
 
-            $this->updateBilan($bilans, $amount, $participants, $payer, $amountByParticipants);
-        }
+            $names = array_unique($names);
 
-        $names = array_unique($names);
-
-        foreach ($bilans as $bilan) {
-            $owes = [];
-            foreach ($names as $name) {
-                if ($name != $bilan->getName()) {
-                    $owes[$name] = 0;
-                }
-            }
-            $bilan->setOwe($owes);
+            $this->updateBilan($bilans, $amount, $participants, $payer, $amountByParticipants, $names);
         }
-        dump($bilans);
     }
 
-    private function updateBilan($bilans, $amount, $participants, $payer, $amountByParticipants)
+    private function updateBilan($bilans, $amount, $participants, $payer, $amountByParticipants, $names)
     {
+
         foreach ($bilans as $bilan) {
+
             $name = $bilan->getName();
             $cost = $bilan->getCost();
-            $owe = $bilan->getOwe();
             $participations = $bilan->getParticipation();
+            $owe = $bilan->getOwe();
+
+
             if ($payer === $name) {
                 $bilan->setCost($cost + $amount);
             }
             if (in_array($name, $participants)) {
                 $bilan->setParticipation($participations + $amountByParticipants);
             }
+
+            foreach ($participants as $participant) {
+                $bilanName = $bilan->getname();
+                $otherOwe = $bilans[$participant]->getOwe();
+
+                if ($bilanName !== $payer) {
+                    break;
+                }
+
+                if ($bilanName != $participant) {
+                    if (array_key_exists($participant, $owe)) {
+                        $owe[$participant] += $amountByParticipants;
+                    } else {
+                        $owe[$participant] = $amountByParticipants;
+                    }
+                }
+                $otherOwe[$bilanName] = -$amountByParticipants;
+                $bilan->setOwe($otherOwe);
+                //Exemple premiere dépense, Alice paye 3 euros pour tous
+                // Donc pour bilan -> getOwe , j'initialise setOwe + amount dépense 
+                // Donc dans le bilans[$participant] -> getOwe 
+                // le owe de la personne owe[$bilanName] -= $amountByParticipant
+                // Pour $owe[$participant] , faire $bilan->getOwe[]
+            }
+
+            $bilan->setOwe($owe);
+
+            // Alice 
+            // owe => Charles, 300 , Camille , 300
+
+            // Charles et Camille doivent 300 à Alice
+
+            // Charles 
+            // owe => Alice, 600 , Camille , 600
+
+            // Alice et Camille doivent 600 à Charles
+
+            // Camille 
+            // owe => Alice, 1200 , Charles , 1200
+
+            //Alice et Charles doivent 1200 à Camille
+
+            //Donc
+            // Alice doit 1200 - 300 à Camille = 900 
+            // Alice doit 600 - 300 a Charles = 300
+
+            // Charles doit 300 - 600 à Alice = - 300
+            // Charles doit 1200 - 600 à Camille = 600
+
+            // Camille doit 300 - 1200 à Alice = - 900
+            // Camille doit 600 - 1200 à Charles = - 600
+
+            // Mais comme Alice doit 300 a Charles, on répercute a Camille, donc Alice devra -900 - 300 à Camille et Charles - 600 + 300
+            // Donc Alice doit 1200 a Camille et Charles doit 300 à Camille
         }
     }
 }
