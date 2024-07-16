b717c69bf92e577e54f9b360bc2ecbd6cf66f97e
Leo Grouet <leo.grouet@gmail.com>
2024-07-16 11:44:24 +0200

    feat: migration merge

--- App/migrations/Version20240716094310.php
+++ App/migrations/Version20240716094310.php
@@ -7,20 +7,25 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20240710095109 extends AbstractMigration
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240716094310 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add password to User Entity';
+        return 'Add password field to user table, and create unique index on email field';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE "user" ADD password VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
     }
 
     public function down(Schema $schema): void
     {
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74');
         $this->addSql('ALTER TABLE "user" DROP password');
     }
 }
