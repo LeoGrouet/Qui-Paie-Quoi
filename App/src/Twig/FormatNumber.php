cc01733a6e3a7245709bbf8088715a5387c7c30d
Leo Grouet <leo.grouet@gmail.com>
2024-07-29 14:29:01 +0200

    fix: deprecated twig extension

--- App/src/Twig/FormatNumber.php
+++ App/src/Twig/FormatNumber.php
@@ -7,7 +7,7 @@ use Twig\TwigFunction;
 
 class FormatNumber extends AbstractExtension
 {
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new TwigFunction('formatNumber', [$this, 'formatNumber']),
