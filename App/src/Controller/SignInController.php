ef395186ae8524ae44bb1c996ba5204273baaaca
Leo Grouet <leo.grouet@gmail.com>
2024-07-31 17:19:39 +0200

    feat: login and logout functionnal

--- App/src/Controller/SignInController.php
+++ App/src/Controller/SignInController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -13,21 +12,11 @@ class SignInController extends AbstractController
     #[Route('/signin', name: 'signin', methods: ['GET', 'POST'])]
     public function signup(
         AuthenticationUtils $authenticationUtils,
-        Request $request
     ): Response {
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        $lastUsername = $authenticationUtils->getLastUsername();
-
         return $this->render('register/signin.html.twig', [
-            'last_username' => $lastUsername,
             'error' => $error,
         ]);
-    }
-
-    #[Route('/signout', name: 'signout', methods: ['GET'])]
-    public function signout(): Response
-    {
-        return $this->redirectToRoute('home');
     }
 }
